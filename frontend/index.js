5b96785dff0f509c45ab9cc613d33eb6655531fd
Kadiyala Dhanush <kadiyaladhanush@gmail.com>
2026-08-14 10:47:10 +0530

    completed with API setup

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,7 @@ const toggleBtn = document.getElementById('toggleBtn');
 var nameElement = document.getElementById('name');
 var genderElement = document.getElementById('gender');
 var imgElement = document.getElementById('img');
+
 const arr = [{
     "name": "John",
     "gender": "male",
@@ -11,10 +12,27 @@ const arr = [{
     "gender": "female",
     "img": "jane.png"
 }]
+
 var curr = 0;
 function toggleUser() {
     curr = (curr + 1) % arr.length;
     nameElement.textContent = arr[curr].name;
     genderElement.textContent = arr[curr].gender;
     imgElement.src = arr[curr].img;
+}
+
+ function randomUser(){
+     fetch("https://randomuser.me/api")
+        .then((result)=>{
+            return result.json();
+        })
+        .then((result)=>{
+            nameElement.textContent = result.results[0].name.first + " " + result.results[0].name.last;
+            genderElement.textContent = result.results[0].gender;
+            imgElement.src = result.results[0].picture.large;
+        })
+        .catch((error)=>{
+            nameElement.textContent = "Error fetching user";
+            console.log(error.data);
+        })
 }
--- server.js
+++ server.js
@@ -6,3 +6,5 @@ app.use(express.static("frontend"));
 app.listen(4000,()=>{
     console.log("Server is running on port 4000 ✅")
 })
+
+
